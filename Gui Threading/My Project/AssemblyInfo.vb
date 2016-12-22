7530c97f04d2ddd3d44513cf3f07c82c13b21269
Jay Asbury <vbjay.net@gmail.com>
2016-12-22 00:51:52 -0500

    Initial project

---
+++ Gui Threading/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Gui Threading")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("VBJay Solutions")>
+<Assembly: AssemblyProduct("Gui Threading")>
+<Assembly: AssemblyCopyright("Copyright © VBJay Solutions 2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("dff422a9-1212-4cee-9298-3c35fcf4bdc6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
